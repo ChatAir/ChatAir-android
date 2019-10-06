2f4606b382214e773902de15339632e7680033aa
Adeel <adeelzafar619@gmail.com>
2019-10-06 19:17:19 +0500

    Reformat project using Square

--- app/src/main/java/com/chatair_android/MainActivity.kt
+++ app/src/main/java/com/chatair_android/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 }
