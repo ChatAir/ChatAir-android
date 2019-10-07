58e88116fdc69c243f951355e297fc28058803ed
killbotxd <sonawane.1@iitj.ac.in>
2019-10-08 04:26:27 +0530

    add top bar according to design change Text view to edit text

---
+++ app/src/main/java/com/chatair_android/UserProfileActivity.kt
@@ -0,0 +1,17 @@
+package com.chatair_android
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserProfileActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_profile_screen)
+
+
+
+    }
+
+}
