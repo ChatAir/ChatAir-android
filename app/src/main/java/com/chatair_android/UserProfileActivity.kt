5bf6cdaf6db3604cbf036c804abacaacb2f899b1
killbotxd <sonawane.1@iitj.ac.in>
2019-10-08 21:44:59 +0530

    change code style

--- app/src/main/java/com/chatair_android/UserProfileActivity.kt
+++ app/src/main/java/com/chatair_android/UserProfileActivity.kt
@@ -1,11 +1,9 @@
 package com.chatair_android
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class UserProfileActivity:AppCompatActivity(){
+class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_profile_screen)
