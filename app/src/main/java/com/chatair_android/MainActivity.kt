bbdcce219e530b6cc924e2ebe25d731f58437f41
Max Hvesser <maxhvesser@gmail.com>
2019-10-13 20:01:47 +0200

    #8 Implementation of Chat Screen UI as per design.

--- app/src/main/java/com/chatair_android/MainActivity.kt
+++ app/src/main/java/com/chatair_android/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.chatair_android.ui.screens.main
+package com.chatair_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.chatair_android.R
 
 class MainActivity : AppCompatActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
+
 }
--- app/src/main/java/com/chatair_android/utils/Ktx.kt
+++
@@ -1,9 +0,0 @@
-package com.chatair_android.utils
-
-import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-
-fun AppCompatActivity.gotoActivity(context: Context, cls: Class<*>) {
-    this.startActivity(Intent(context, cls))
-}
