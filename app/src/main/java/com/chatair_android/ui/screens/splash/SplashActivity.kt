44fb9c9bcee9af8e499aae49ab7f23bb7a219a0e
alistair <alistairholmes95@gmail.com>
2019-10-20 12:04:34 +0200

    #5 Removed hardcoded widths for logo and launch login activity after splash activity

--- app/src/main/java/com/chatair_android/ui/screens/splash/SplashActivity.kt
+++ app/src/main/java/com/chatair_android/ui/screens/splash/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.chatair_android.ui.screens.splash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.chatair_android.ui.screens.main.MainActivity
+import com.chatair_android.LoginActivity
 import com.chatair_android.R
 import com.chatair_android.utils.gotoActivity
 
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            this.gotoActivity(this@SplashActivity, MainActivity::class.java)
+            this.gotoActivity(this@SplashActivity, LoginActivity::class.java)
         }, 4000)
     }
 }
